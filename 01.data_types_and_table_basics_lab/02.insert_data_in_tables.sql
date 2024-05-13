212d651ef2bda96b52eb520688759e3648596cfa
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-05-13 17:07:29 -0300

    Add files via upload

---
+++ 01.data_types_and_table_basics_lab/02.insert_data_in_tables.sql
@@ -0,0 +1,10 @@
+INSERT INTO employees(
+        first_name,
+        last_name,
+        hiring_date,
+        salary,
+        devices_number
+    )
+VALUES ('John', 'Smith', '2024-01-01', 10000.00, 592),
+    ('Jane', 'Doe', '2020-03-15', 5800, 326),
+    ('Gogo', 'Sarev', '2022-01-15', 6300, 315);
