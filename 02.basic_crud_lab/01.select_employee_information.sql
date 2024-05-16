b7aa2a9d311caebb6752b0efc81d6d1802d96490
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-05-16 11:21:04 -0300

    Add files via upload

---
+++ 02.basic_crud_lab/01.select_employee_information.sql
@@ -0,0 +1,9 @@
+SELECT id, 
+concat(first_name, ' ', last_name) AS "Full Name", 
+job_title AS "Job Title" 
+FROM employees;
+
+/*SELECT id,
+    first_name || ' ' || last_name AS "Full Name",
+    job_title AS "Job Title"
+FROM employees;*/
