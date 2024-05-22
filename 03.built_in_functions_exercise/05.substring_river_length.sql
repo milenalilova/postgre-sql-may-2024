07294472a9de074b5df25c1de5accdc2481f2d64
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-05-22 09:08:06 -0300

    Add files via upload

---
+++ 03.built_in_functions_exercise/05.substring_river_length.sql
@@ -0,0 +1,9 @@
+SELECT 
+	SUBSTRING("River Information", '[0-9]{1,4}') AS river_length
+FROM 
+	view_river_info;
+
+--SELECT 
+--	(REGEXP_MATCHES("River Information", '[0-9]{1,4}'))[1] AS river_length
+--FROM 
+--	view_river_info;    
