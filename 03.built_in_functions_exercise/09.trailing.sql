b1da889352200517f916291b4db6b3b5d90760e9
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-05-22 09:58:49 -0300

    Add files via upload

--- 03.built_in_functions_exercise/09.trailing.sql
+++ 03.built_in_functions_exercise/09.trailing.sql
@@ -2,3 +2,8 @@ SELECT
 	continent_name,
 	TRIM(TRAILING FROM continent_name) AS trim
 FROM continents;
+
+--SELECT 
+--	continent_name,
+--	TRIM(LEADING FROM continent_name) AS "trim"
+--FROM continents;
