832b3f032dde3b481d69534ba3dc36dd36bc6af4
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-05-24 11:10:00 -0300

    Add files via upload

--- 04.data_aggregation_exercise/09.notes_with_dumbledore.sql
+++ 04.data_aggregation_exercise/09.notes_with_dumbledore.sql
@@ -5,3 +5,13 @@ FROM wizard_deposits
 WHERE notes LIKE('%Dumbledore%')
 GROUP BY last_name
 ORDER BY last_name;
+
+/*
+SELECT
+	last_name,
+	COUNT(*) AS notes_with_dumbledore
+FROM wizard_deposits
+WHERE notes LIKE('%Dumbledore%')
+GROUP BY last_name
+ORDER BY last_name;
+*/
