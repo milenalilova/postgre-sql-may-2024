810b9a2a70489b35cb755e83035bdcefcb14b4cc
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-05-25 16:03:45 -0300

    Add files via upload

---
+++ 05.table_relations_lab.sql/04.delete_mountains.sql
@@ -0,0 +1,15 @@
+CREATE TABLE mountains(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE peaks(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	mountain_id INT,
+	CONSTRAINT fk_mountain_id
+		FOREIGN KEY (mountain_id)
+			REFERENCES mountains(id)
+				ON DELETE CASCADE
+	
+);
