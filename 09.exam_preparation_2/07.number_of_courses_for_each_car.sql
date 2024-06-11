12b8d277e198d94baac084851a26cfe4857a201e
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-06-11 15:31:58 -0300

    Add files via upload

---
+++ 09.exam_preparation_2/07.number_of_courses_for_each_car.sql
@@ -0,0 +1,21 @@
+SELECT
+	c.id AS car_id,
+	c.make,
+	c.mileage,
+	COUNT(co.id) count_of_courses,
+	ROUND(AVG(co.bill), 2) AS average_bill
+	
+FROM 
+	cars AS c
+LEFT JOIN 
+	courses AS co
+ON co.car_id = c.id
+GROUP BY 
+	c.id,
+	c.make,
+	c.mileage
+HAVING
+	COUNT(c.id) != 2
+ORDER BY 
+	count_of_courses DESC,
+	c.id ASC;
