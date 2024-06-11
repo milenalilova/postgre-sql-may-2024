b6567554305a656ebe93b9d44330bb97d89cff83
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-06-11 11:24:22 -0300

    Add files via upload

---
+++ 08.exam_preparation_1/12.animals_with_owner_or_not.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE PROCEDURE sp_animals_with_owners_or_not(
+	IN animal_name VARCHAR(30),
+	OUT result VARCHAR(30)
+	)
+AS 
+$$
+	BEGIN
+		SELECT 
+			o.name
+		INTO result 
+		FROM
+			animals AS a
+		JOIN 
+			owners AS o
+		ON o.id = a.owner_id
+		WHERE a."name" = animal_name;
+		IF result IS NULL THEN result = 'For adoption'; END IF;
+	END;
+$$
+LANGUAGE plpgsql;
+
+
+/*CREATE OR REPLACE PROCEDURE sp_animals_with_owners_or_not(
+    IN animal_name VARCHAR(30),
+    OUT result VARCHAR(30)
+)
+AS 
+$$
+BEGIN
+    SELECT 
+        COALESCE(o.name, 'For adoption')
+    INTO result 
+    FROM
+        animals AS a
+    LEFT JOIN 
+        owners AS o
+    ON o.id = a.owner_id
+    WHERE a."name" = animal_name;
+END;
+$$
+LANGUAGE plpgsql;*/
